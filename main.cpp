6d156361993930a3d47dece4971706f81e785626
Maya <mvenega9@my.chemeketa.edu>
2024-05-25 03:28:17 +0000

    May 24, 2024, 8:28 PM

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
+#include <ifstream>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-  char board[9][9];
+  int boardColumns = 9;
+  int boardRows = 9;
+  char board[boardColumns][boardRows]{};
+  ifstream file;
+  file.open("go.txt");
+
+  for (int i = 0; i < boardRows; ++i) {
+    for (int j = 0; i < boardColumns; ++i) {
+      cin <<
+    }
+  }
 }
