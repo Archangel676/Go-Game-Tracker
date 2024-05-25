09c6be5fff9d1556078511c0f3744089c3ddba1e
Maya <mvenega9@my.chemeketa.edu>
2024-05-25 04:22:33 +0000

    May 24, 2024, 9:22 PM

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
   ifstream file;
   file.open("go.txt");
 
+  // filling to board
   for (int i = 0; i < 9; ++i) {   // rows
     for (int j = 0; j < 9; ++j) { // column
       file >> board[i][j];
@@ -18,10 +19,22 @@ int main()
     cout << endl;
   }
 
+  // getting starting location
   int startLocationX, startLocationY;
   cin >> startLocationX >> startLocationY;
   cout << "Start location is: (" << startLocationX << ", " << startLocationY
        << ")";
 
   return 0;
+
+  // TO-DO
+  // Your program must make use of recursion for determining the size of the
+  // group and number of liberties. Your program should then read in a row and
+  // column from the console indicating a starting location. It should print
+  // out: 1) the board, with the squares that are in the group made uppercase
+  // and the liberties for the group marked as *'s;
+  // 2) the number of pieces in the group based at the starting location;
+  // 3) how many liberties there are.
+  // If the row, col the user enters does not have a piece in it (is a - instead
+  // of w or b), print an error message.
 }
