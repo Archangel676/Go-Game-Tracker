d225196712f3deeb821c29cd1acd7f865ebbd988
Maya <mvenega9@my.chemeketa.edu>
2024-05-28 05:38:44 +0000

    May 27, 2024, 10:38 PM

--- main.cpp
+++ main.cpp
@@ -104,17 +104,7 @@ int main()
 
   return 0;
 
-  // TO-DO
-  // Use recursion for determining the size of the group and number of liberties
-
-  // It should print out:
-  //  1) the board, with the squares that are in the group made uppercase
   //  and the liberties for the group marked as *'s;
 
-  // 2) the number of pieces in the group based at the starting location;
-
   // 3) how many liberties there are.
-
-  // If the row, col the user enters does not have a piece in it (is a - instead
-  // of w or b), print an error message.
 }
